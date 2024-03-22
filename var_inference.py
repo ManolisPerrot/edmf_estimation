3752d2f496d9f5759446cc47c4ed60f0631b0058
manolis.perrot <manolis.perrot@univ-grenoble-alpes.fr>
2024-03-22 17:46:08 +0100

    new juliacall package

--- var_inference.py
+++ var_inference.py
@@ -1,42 +1,43 @@
-from julia.api import Julia
-jl = Julia(compiled_modules=False)
+#from julia.api import Julia
+from juliacall import Main as jl
+#jl = Julia(compiled_modules=False)
 
 from likelihood_mesonh import likelihood_mesonh
 import time
 
 ## install Julia packages
-jl.eval("using Pkg")
-jl.eval("""Pkg.add(url="https://github.com/benjione/SequentialMeasureTransport.jl.git")""")
-jl.eval("""Pkg.add("ApproxFun")""")
-jl.eval("""Pkg.add("Hypatia")""")
+jl.seval("using Pkg")
+jl.seval("""Pkg.add(url="https://github.com/benjione/SequentialMeasureTransport.jl.git")""")
+jl.seval("""Pkg.add("ApproxFun")""")
+jl.seval("""Pkg.add("Hypatia")""")
 
 ## import Julia packages
-jl.eval("using SequentialMeasureTransport")
-jl.eval("import SequentialMeasureTransport as SMT")
-jl.eval("using ApproxFun")
-jl.eval("using Hypatia")
-from julia import Main
+jl.seval("using SequentialMeasureTransport")
+jl.seval("import SequentialMeasureTransport as SMT")
+jl.seval("using ApproxFun")
+jl.seval("using Hypatia")
+#from julia import Main
 
 ## Determine parameter ranges
 likelihood = lambda x: likelihood_mesonh(Cent=x[0], Cdet=x[1], wp_a=x[2], wp_b=x[3], wp_bp=x[4], up_c=x[5], bc_ap=x[6], delta_bkg=x[7])
 
 # create everything needed for the model
-jl.eval("""
-L = zeros(8) # lower bounds of parameters
-R = ones(8) # upper bounds of parameters
+jl.seval("""
+L = [0.0, 1, 0, 0, 0, 0, 0, 0] # lower bounds of parameters
+R = [1.0, 2, 1, 1, 10, 1, 1, 10] # upper bounds of parameters
 N = 8
 reference_map = SMT.ScalingReference{N}(L, R)
 model = PSDModel(Legendre(0.0..1.0)^(N), :downward_closed, 5, max_Φ_size=50)
 """)
-Main.likelihood = likelihood        # set the likelihood function in Julia
-jl.eval("likelihood_func(x) = 1000.0*likelihood(x)")  # make pyobject a function in Julia (not threadsafe!!)
+jl.likelihood = likelihood        # set the likelihood function in Julia
+jl.seval("likelihood_func(x) = Float64(1000.0*likelihood(x))")  # make pyobject a function in Julia (not threadsafe!!)
 ## create the sampler
-sra_sampler = jl.eval("""
+sra_sampler = jl.seval("""
 sra_chi2 = SMT.SelfReinforcedSampler(likelihood_func, model, 4, :Chi2,
                         reference_map; trace=true,
                         ϵ=1e-6, λ_2=0.0, λ_1=0.0,
                         algebraic_base=2.0,
-                        N_sample=10000,
+                        N_sample=100,
                         threading=false,  # threading can not be used with pyobject function
                         # optimizer=Hypatia.Optimizer
                     )
