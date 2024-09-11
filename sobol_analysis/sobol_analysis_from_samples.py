08c4014818a316a87b3ac5e67f58932bf12ae01f
manolis.perrot <manolis.perrot@univ-grenoble-alpes.fr>
2024-09-11 10:19:44 +0200

    Jansen 99 estimator total index /!\ openturns is false, use Prieur' book + fix 'u' saving bug

--- sobol_analysis/sobol_analysis_from_samples.py
+++ sobol_analysis/sobol_analysis_from_samples.py
@@ -29,24 +29,18 @@ start = TIME.time() #monitor duration of execution
 
 ### WARNING: small_ap = False causes many divergent values (theta > 10^50) or NaNs
 
-N = 512
-# cases=['FC500','W005_C500_NO_COR']
-cases=['FC500']
+
+# nsamples = [512,1024,2048,4096]
+nsamples = [2048]
+
+
+cases=['W005_C500_NO_COR']
+additional_attribute='beta1_ap0_'
+# cases=['FC500']
 case = cases[0] 
-fields = ['temp']
+fields = ['temp','u']
 
-samples={}
-
-with open('outputs/samples_FC500_'+str(N), 'rb') as handle:
-    # with open('outputs/sobol_'+nsample, 'rb') as handle:
-    samples = pickle.load(handle)
-
-saving_name = 'sobol_'+cases[0]+'_'+str(N)
-print(saving_name)
-
-nvar = len(samples['variables_range'])
-time = {case: samples[case]['coordinates']['time'][case] for case in cases}
-variables = samples['variables_range']
+#======================================== FUNCTIONS =============================================================
     
 ### Define a L2 inner product because output of the model are functions    
 def product (T1,T2, z_r, time):
@@ -61,8 +55,12 @@ def l2_enumerators(Y,Y_prime,Y_tilde,meanY,meanY_tilde, denominator_l2, discarde
     
     enumerator_l2 = 1/(N-discarded_samples_tilde-discarded_samples) * sum( product(Y[i],Y_tilde[i],z_r,time) for i in range(N) ) - MeanSquaredL2
     # enumerator_total_l2 = denominator_l2 - 1/(N-discarded_samples_tilde-discarded_samples_prime) * sum( product(Y_prime[i],Y_tilde[i],z_r,time) for i in range(N) ) + MeanSquaredL2   
-    enumerator_total_l2 =  1/(N-discarded_samples_tilde-discarded_samples_prime) * sum( product(Y_prime[i],Y_tilde[i],z_r,time) for i in range(N) ) - MeanSquaredL2   
 
+    ### Classic
+    # enumerator_total_l2 =  1/(N-discarded_samples_tilde-discarded_samples_prime) * sum( product(Y_prime[i],Y_tilde[i],z_r,time) for i in range(N) ) - MeanSquaredL2   
+
+    # # Jansen 99
+    enumerator_total_l2 =  1/(2*(N-discarded_samples_tilde-discarded_samples_prime)) * sum( product((Y[i]-Y_tilde[i]),(Y[i]-Y_tilde[i]),z_r,time) for i in range(N) ) 
 
     return enumerator_l2, enumerator_total_l2      
 
@@ -71,7 +69,14 @@ def z_enumerators(Y,Y_prime,Y_tilde,meanY,meanY_tilde,denominator_z, discarded_s
     MeanSquared_z= meanY[:,-1] * meanY_tilde[:,-1]
 
     enumerator_z =  1/(N-discarded_samples_tilde-discarded_samples) * sum( (Y[i][:,-1] * Y_tilde[i][:,-1]) for i in range(N) ) - MeanSquared_z
-    enumerator_total_z = denominator_z - 1/(N-discarded_samples_tilde-discarded_samples_prime) * sum( (Y_prime[i][:,-1] * Y_tilde[i][:,-1]) for i in range(N) ) + MeanSquared_z
+    # enumerator_total_z = denominator_z - 1/(N-discarded_samples_tilde-discarded_samples_prime) * sum( (Y_prime[i][:,-1] * Y_tilde[i][:,-1]) for i in range(N) ) + MeanSquared_z
+    
+    # Classic
+    # enumerator_total_z =  1/(N-discarded_samples_tilde-discarded_samples_prime) * sum( (Y_prime[i][:,-1] * Y_tilde[i][:,-1]) for i in range(N) ) - MeanSquared_z
+
+    # Jansen 99
+    enumerator_total_z =  1/(2*(N-discarded_samples_tilde-discarded_samples_prime)) * sum( ((Y[i][:,-1]-Y_tilde[i][:,-1])**2) for i in range(N) ) 
+    
     return enumerator_z, enumerator_total_z    
 
 def discard_NaNs(Y):
@@ -84,63 +89,80 @@ def discard_NaNs(Y):
 
 #============================ MAIN LOOP =========================================
 
-# initialize output
-output={'variables_range': variables}
-
-for case in cases:
-    z_r = samples[case]['coordinates']['z_r']
-    for field in fields:
-        print('Analysis of the case '+case)
-        Y = samples[case]['variables'][field]['Y']
-        Y, discarded_samples = discard_NaNs(Y)
-        #
-        Y_prime = samples[case]['variables'][field]['Y_prime']
-        Y_prime, discarded_samples_prime = discard_NaNs(Y_prime)
 
 
-        meanY = 1/(N-discarded_samples) *sum(Y[i] for i in range(N))
-        denominator_l2 = 1/(N-discarded_samples) * sum( product(Y[i],Y[i],z_r,time[case]) for i in range(N) ) - product(meanY,meanY,z_r,time[case])
-        denominator_z = 1/(N-discarded_samples) * sum( (Y[i][:,-1])**2 for i in range(N) ) - (meanY[:,-1])**2
+for N in nsamples:
+    samples={}
 
-        S = {'sobol_indices':{}, 'z_r': z_r}
+    with open('outputs/samples_'+additional_attribute+cases[0]+'_'+str(N), 'rb') as handle:
+        samples = pickle.load(handle)
 
-        for parameter in samples['X_tilde']:
+    saving_name = 'sobol_'+additional_attribute+cases[0]+'_'+str(N)
+    print(saving_name)
 
-            Y_tilde = samples[case]['variables'][field]['Y_tilde'][parameter]
-
-            Y_tilde, discarded_samples_tilde = discard_NaNs(Y_tilde) 
-            meanY_tilde =   1/(N-discarded_samples_tilde) *sum(Y_tilde[i] for i in range(N))
-
-            enumerator_l2, enumerator_total_l2 = l2_enumerators(Y,Y_prime,Y_tilde,meanY,meanY_tilde,denominator_l2, discarded_samples,discarded_samples_prime,discarded_samples_tilde,z_r,time[case])
-
-            enumerator_z, enumerator_total_z = z_enumerators(Y,Y_prime,Y_tilde,meanY,meanY_tilde, denominator_z, discarded_samples,discarded_samples_prime,discarded_samples_tilde)
+    nvar = len(samples['variables_range'])
+    time = {case: samples[case]['coordinates']['time'][case] for case in cases}
+    variables = samples['variables_range']
 
 
-            S['sobol_indices'][parameter] = {field: 
-                                        {'l2 index': enumerator_l2/denominator_l2,
-                                        'enumerator_l2': enumerator_l2,
-                                        'denominator_l2': denominator_l2,
-                                        'enumerator_total_l2':enumerator_total_l2,
-                                        # 'total l2 index': enumerator_total_l2/denominator_l2,
-                                        'total l2 index': 1 - enumerator_total_l2/denominator_l2,
+    # initialize output
+    output={'variables_range': variables}
 
-                                        'z index': enumerator_z/denominator_z,
-                                        'enumerator_z':enumerator_z,
-                                        'denominator_z': denominator_z,
-                                        'enumerator_total_z': enumerator_total_z,
-                                        'total z index': enumerator_total_z/denominator_z,
-                                        'discarded_samples': discarded_samples,
-                                        'discarded_samples_tilde': discarded_samples_tilde,
-                                        'discarded_samples_prime': discarded_samples_prime    
-                                        }
-                                        }
+    for case in cases:
+        z_r = samples[case]['coordinates']['z_r']
+        S = {'sobol_indices':{parameter: {} for parameter in samples['X_tilde'] }, 'z_r': z_r}
 
-    output[case] = S
+        for field in fields:
+            print('Analysis of the case '+case)
+            Y = samples[case]['variables'][field]['Y']
+            Y, discarded_samples = discard_NaNs(Y)
+            #
+            Y_prime = samples[case]['variables'][field]['Y_prime']
+            Y_prime, discarded_samples_prime = discard_NaNs(Y_prime)
 
-#==========================
-# Save output
-with open('outputs/'+saving_name, 'wb') as handle:
-    pickle.dump(output, handle, protocol=pickle.HIGHEST_PROTOCOL)
-print('Done! Output saved at '+'outputs/'+saving_name)
-stop = TIME.time()
-print('duration of execution', stop-start)
+
+            meanY = 1/(N-discarded_samples) *sum(Y[i] for i in range(N))
+            denominator_l2 = 1/(N-discarded_samples) * sum( product(Y[i],Y[i],z_r,time[case]) for i in range(N) ) - product(meanY,meanY,z_r,time[case])
+            denominator_z = 1/(N-discarded_samples) * sum( (Y[i][:,-1])**2 for i in range(N) ) - (meanY[:,-1])**2
+
+
+            for parameter in samples['X_tilde']:
+
+                Y_tilde = samples[case]['variables'][field]['Y_tilde'][parameter]
+
+                Y_tilde, discarded_samples_tilde = discard_NaNs(Y_tilde) 
+                meanY_tilde =   1/(N-discarded_samples_tilde) *sum(Y_tilde[i] for i in range(N))
+
+                enumerator_l2, enumerator_total_l2 = l2_enumerators(Y,Y_prime,Y_tilde,meanY,meanY_tilde,denominator_l2, discarded_samples,discarded_samples_prime,discarded_samples_tilde,z_r,time[case])
+
+                enumerator_z, enumerator_total_z = z_enumerators(Y,Y_prime,Y_tilde,meanY,meanY_tilde, denominator_z, discarded_samples,discarded_samples_prime,discarded_samples_tilde)
+
+
+                S['sobol_indices'][parameter][field] ={  
+                                            'l2 index': enumerator_l2/denominator_l2,
+                                            'enumerator_l2': enumerator_l2,
+                                            'denominator_l2': denominator_l2,
+                                            'enumerator_total_l2': enumerator_total_l2,
+                                            # 'total l2 index': enumerator_total_l2/denominator_l2,
+                                            'total l2 index': 1-enumerator_total_l2/denominator_l2,
+
+                                            'z index': enumerator_z/denominator_z,
+                                            'enumerator_z':enumerator_z,
+                                            'denominator_z': denominator_z,
+                                            'enumerator_total_z': enumerator_total_z,
+                                            'total z index': 1-enumerator_total_z/denominator_z,
+                                            'discarded_samples': discarded_samples,
+                                            'discarded_samples_tilde': discarded_samples_tilde,
+                                            'discarded_samples_prime': discarded_samples_prime    
+                                            }
+                                            
+
+        output[case] = S
+
+    #==========================
+    # Save output
+    with open('outputs/'+saving_name, 'wb') as handle:
+        pickle.dump(output, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print('Done! Output saved at '+'outputs/'+saving_name)
+    stop = TIME.time()
+    print('duration of execution', stop-start)
