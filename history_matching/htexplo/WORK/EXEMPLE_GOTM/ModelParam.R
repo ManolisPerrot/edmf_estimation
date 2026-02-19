a3459e360ec896dfc6aa4de8889d1176d2a05b17
manolis.perrot <manolis.perrot@univ-grenoble-alpes.fr>
2026-02-19 18:02:00 +0100

    change models params file for ket

--- history_matching/htexplo/WORK/EXEMPLE_GOTM/ModelParam.R
+++ history_matching/htexplo/WORK/EXEMPLE_GOTM/ModelParam.R
@@ -1,9 +1,9 @@
-NPARA=9
-param.names=c("Cent","Cdet","wp_a","wp_b","wp_bp","up_c","bc_ap","delta_bkg","wp0")
-param.lows=c(0.,1.,0.,0.,0.,0.,0.,0.,1e-8)
-param.highs=c(1.,2.,1.,1.,3.,1.,0.45,3.,1e-1)
-param.defaults=c(0.9,1.7,0.9,0.9,2.,0.5,0.2,2.,0.5e-7)
-which.logs<-c(9)
+NPARA=10
+param.names=c("cc1","cc2","cc3","cc4","cc6","ct1","ct2","ct3","ct5","ctt")
+param.lows=c(2.5000,0.4000,1.0000,0.5360,0.2000,2.9500,0.3000,0.5000,0.1533,0.3000)
+param.highs=c(10.000,1.6000,4.0000,2.1360,0.8000,12.500,1.2000,2.0000,0.6633,1.4000)
+param.defaults=c(5.0000,0.8000,1.9680,1.1360,0.4000,5.9500,0.6000,1.0000,0.3333,0.7200)
+which.logs<-c()
   param.defaults <- param.defaults[1:NPARA]
   param.highs <- param.highs[1:NPARA]
   param.lows <- param.lows[1:NPARA]
